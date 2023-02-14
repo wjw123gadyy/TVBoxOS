76fea7ffc2f8e7d88e10a23fd4c40fc3800e6ad0
yangqiang3504 <412594121@qq.com>
2023-02-14 16:11:51 +0800

    fix - push time2

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -142,8 +142,8 @@ public class VodController extends BaseController {
     private Runnable myRunnable2 = new Runnable() {
         @Override
         public void run() {
-            String ctime = mCurrentTime.getText();
-            String etime = mTotalTime.getText();
+            String ctime = mCurrentTime.getText().toString();
+            String etime = mTotalTime.getText().toString();
             tvTime.setText(ctime+"/"+etime);
             Date date = new Date();
             SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");

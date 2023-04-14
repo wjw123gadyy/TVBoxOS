405c385ab02c25a10f86c2a143200aa990750eae
yangqiang4125 <412594121@qq.com>
2023-04-15 03:15:42 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -977,6 +977,13 @@ public class VodController extends BaseController {
             fromLongPress = true;
             try {
                 float speed2 = (float) mPlayerConfig.getDouble("sp");
+                int current = (int) mControlWrapper.getCurrentPosition();
+                if (speed2 == 1.0f&&current==0) { 
+                    speed2 == 1.5f;
+                    mPlayerConfig.put("st", 110);
+                    mPlayerConfig.put("et", 150);
+                }
+                
                 if (speed2 != 3.0f) {
                     speed_old = speed2;
                     float speed = 3.0f;

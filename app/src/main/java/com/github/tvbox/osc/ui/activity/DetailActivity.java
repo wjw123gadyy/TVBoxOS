d0b3c251b2ce592ec450c5d8899ae7e3957aa47b
yangqiang3504 <412594121@qq.com>
2023-04-17 11:38:07 +0800

    fix - FastSearchActivity m11

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -160,9 +160,6 @@ public class DetailActivity extends BaseActivity {
         initData();
     }
 
-    public static void start(Context context, String key, String id, String name) {
-        start(context,key,id,name,"");
-    }
     public static void start(Activity activity, String key, String id, String name, String pic) {
         Intent newIntent = new Intent(activity, DetailActivity.class);
         newIntent.putExtra("wdName", name);
@@ -172,6 +169,10 @@ public class DetailActivity extends BaseActivity {
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         activity.startActivity(newIntent);
     }
+
+   /* public static void start(Context context, String key, String id, String name) {
+        start(context,key,id,name,"");
+    }
     public static void start(Context context, String key, String id, String name, String pic) {
         Intent newIntent = new Intent(context, DetailActivity.class);
         newIntent.putExtra("wdName", name);
@@ -180,7 +181,7 @@ public class DetailActivity extends BaseActivity {
         newIntent.putExtra("pic", pic);
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         context.startActivity(newIntent);
-    }
+    }*/
 
     private void initView() {
         llLayout = findViewById(R.id.llLayout);

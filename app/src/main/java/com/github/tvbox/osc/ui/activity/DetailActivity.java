dc6aac99ff74dc924e06ac249efd16bca7bc0b5a
yangqiang3504 <412594121@qq.com>
2023-05-12 11:03:31 +0800

    fix - 历史缓存112233

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -861,6 +861,7 @@ public class DetailActivity extends BaseActivity {
                                 vodInfo.playFlag = vodInfoRecord.playFlag;
                                 vodInfo.playerCfg = vodInfoRecord.playerCfg;
                                 vodInfo.reverseSort = vodInfoRecord.reverseSort;
+                                App.getInstance().setVodInfo(vodInfo);
                             } else {
                                 vodInfo.playIndex = 0;
                                 vodInfo.playFlag = null;

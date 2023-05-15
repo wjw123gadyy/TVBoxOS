c94e7e674752908d0c83ed4e664640a09eea0b06
yangqiang3504 <412594121@qq.com>
2023-05-15 09:39:37 +0800

    fix - 历史缓存记录112

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -856,7 +856,6 @@ public class DetailActivity extends BaseActivity {
 
                             // 读取历史记录
                             if (vodInfoRecord != null) {
-                                vodInfo.oid = vodInfoRecord.oid;
                                 vodInfo.playIndex = Math.max(vodInfoRecord.playIndex, 0);
                                 vodInfo.playFlag = vodInfoRecord.playFlag;
                                 vodInfo.playerCfg = vodInfoRecord.playerCfg;

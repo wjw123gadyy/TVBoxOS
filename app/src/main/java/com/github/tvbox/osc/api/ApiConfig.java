dfaae76c3e81de3bf19273e1296042fb32171e9e
yangqiang3504 <412594121@qq.com>
2023-06-19 15:01:58 +0800

    fix - home1

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -337,7 +337,7 @@ public class ApiConfig {
                         }
                         Hawk.put(HawkConfig.API_URL, _api);
 
-                        callback.error("拉取配置失败\n" + (response.getException() != null ? response.getException().getMessage()+"  "+apiUrl+ : ""));
+                        callback.error("拉取配置失败\n" + (response.getException() != null ? response.getException().getMessage()+"  "+apiUrl : ""));
                     }
 
                     public String convertResponse(okhttp3.Response response) throws Throwable {

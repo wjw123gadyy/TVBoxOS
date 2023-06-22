657f41942f7bf4abff63458870f582e337110331
yangqiang4125 <412594121@qq.com>
2023-06-22 12:57:26 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -171,7 +171,7 @@ public class VodController extends BaseController {
                 if(mPlayLoadNetSpeed.getVisibility()==VISIBLE){
                     if (v==GONE)speed = PlayerHelper.getDisplaySpeed(mControlWrapper.getTcpSpeed());
                     mPlayLoadNetSpeed.setText(speed);
-                    if (mControlWrapper.getLoadTime() > 6) {
+                    if (v==GONE&&mControlWrapper.getLoadTime() > 6) {
                         mControlWrapper.setLoadTime();
                         listener.replay(false);
                     }
